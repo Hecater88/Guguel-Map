30452352c90ac461d0e165683c4926e57c9fae6f
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2022-02-07 21:50:51 +0100

    List and cards

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import GoogleMapReact from "google-map-react";
-import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOnOutlineIcon from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab";
-import useStyles from "./styles";
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlineIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+import useStyles from './styles';
 
-const Map = () => {
+const Map = ({setCoordinates, setBounds, coordinates}) => {
   const classes = useStyles();
-  const isMobile = useMediaQuery("(min-width:600px)");
-  const coordinates = { lat: 0, lng: 0 };
-
+  const isMobile = useMediaQuery('(min-width:600px)');
+ 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
@@ -18,9 +17,12 @@ const Map = () => {
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={""}
-        onChange={""}
-        onChildClick={""}
+        options={''}
+        onChange={(e)=>{
+          setCoordinates({lat: e.center.lat, lng: e.center.lng})
+          setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+          }}
+        onChildClick={''}
       >
 
       </GoogleMapReact>
