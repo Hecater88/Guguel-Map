941cb63c4f605b6f7645354611487c59cffd9816
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2022-02-17 01:51:03 +0100

    Errors fixed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Map from "./components/Map/Map";
 const App = () => {
   const [places, setPlaces] = useState([]);
   const [coordinates, setCoordinates] = useState({});
-  const [bounds, setBounds] = useState(null);
+  const [bounds, setBounds] = useState({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -38,6 +38,7 @@ const App = () => {
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
           />
         </Grid>
       </Grid>
